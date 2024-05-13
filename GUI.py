24cb9d40327f97863c039e4d844218ee9a702423
LehichR <redkinalex325@gmail.com>
2024-05-13 22:32:06 +0300

    1.0.2

--- GUI.py
+++ GUI.py
@@ -7,11 +7,12 @@ def clear():
     widgets = window.place_slaves()  # список виджетов
     for i in widgets:
         i.destroy()  # удаление виджетов
-    b = Button(text="Домой",font=("Comic Sans",15),command=menu)
+    b = Button(text="Домой", font=("Comic Sans", 15), command=menu)
     b.place(x=25, y=500, width=200)
 
 
 def menu():
+    clear()
     title = Label(text="Что вы выберете?", font=("Comic Sans", 24), fg="white", bg="orange")
     title.place(x=0, y=0, width=700)
     Button(text="Решить уравнение", font=("Arial", 15), command=calc).place(x=250, y=50)
@@ -19,6 +20,16 @@ def menu():
 
 def calc():
     clear()
+    Label(text="Введите ваше уравнение", font=("Arial", 15)).place(x=250, y=50)
+
+    def get_coefficient():
+        a_c = a.get()
+        print(a_c)
+
+    a = Entry(width=5)
+    a.place(x=100, y=100)
+
+    Button(text="Решить", font=("Arial", 15), command=get_coefficient).place(x=250, y=120)
 
 
 window = Tk()
